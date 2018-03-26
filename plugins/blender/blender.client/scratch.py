7c7188043702bd8652abe18f90b94c0bb3ec5fbb
Andrew Scobie <ascobie@microsoft.com>
2018-03-27 11:11:36 +1300

    scratch file

--- plugins/blender/blender.client/scratch.py
+++ plugins/blender/blender.client/scratch.py
@@ -6,15 +6,15 @@ VERSION = "0.2.0"
 
 def main():
     """Scratch Pad"""
-    
-    _regexUnderscoresAndMultipleDashes = r'/[_-]{1,}/g'
+
+    _regexUnderscoresAndMultipleDashes = r'[_-]{1,}'
     _regexTrimStartAndEnd = r'^[-]|[-]+$'
 
     filename = "--my__file--asd_another"
-    print("--my__file--asd_another => " + re.sub(r'[_-]{1,}', "-", filename))
+    print("--my__file--asd_another => " + re.sub(_regexUnderscoresAndMultipleDashes, "-", filename))
 
-    filename = "-start-and-end-"
-    print("-start-and-end- => " + re.sub(r'^[-]|[-]+$', "", filename))
+    #filename = "-start-and-end-"
+    #print("-start-and-end- => " + re.sub(_regexTrimStartAndEnd, "", filename))
 
 
 if __name__ == '__main__':
