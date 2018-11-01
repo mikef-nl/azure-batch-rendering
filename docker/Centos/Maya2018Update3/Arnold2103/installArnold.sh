98301e910ca67ddc0f8fc12b506d162545e42fd6
David Kydd <dakydd@microsoft.com>
2018-11-02 10:49:58 +1300

    Rename arg to INSTALLER_SAS and remove maya-update and renderer-version specifics for ease of use

--- docker/Centos/Maya2018Update3/Arnold2103/installArnold.sh
+++ docker/Centos/Maya2018Update3/Arnold2103/installArnold.sh
@@ -1,20 +1,20 @@
 #!/bin/sh -x
 
-echo 'MTOA_INSTALL_SAS='$1
-MTOA_INSTALL_SAS=$1
+echo 'INSTALLER_SAS='$1
+INSTALLER_SAS=$1
 
 mkdir -m777 /tmp/mtoa
 echo '-------------------------------------------------------------------------------------'
-echo '------------------------------=Download Arnold 2018=---------------------------------'
+echo '------------------------------=Download Arnold=---------------------------------'
 echo '-------------------------------------------------------------------------------------'
 
-azcopy --source $MTOA_INSTALL_SAS --destination /tmp/mtoa/MtoA-2.1.0.3-linux-2018.run
+azcopy --source $INSTALLER_SAS --destination /tmp/mtoa/MtoA-linux-2018.run
 
 echo '-------------------------------------------------------------------------------------'
-echo '------------------------=Extract and Install Arnold 2018=----------------------------'
+echo '------------------------=Extract and Install Arnold=----------------------------'
 echo '-------------------------------------------------------------------------------------'
-7za x /tmp/mtoa/MtoA-2.1.0.3-linux-2018.run -o/tmp/mtoa/
-7za x /tmp/mtoa/MtoA-2.1.0.3-linux-2018 -o/tmp/mtoa/MtoA2018
+7za x /tmp/mtoa/MtoA-linux-2018.run -o/tmp/mtoa/
+7za x /tmp/mtoa/MtoA-linux-2018 -o/tmp/mtoa/MtoA2018
 7za x /tmp/mtoa/MtoA2018/package.zip -o/opt/solidangle/mtoa/2018/
 
 cp /opt/solidangle/mtoa/2018/arnoldRenderer.xml /usr/autodesk/mayaIO2018/bin/rendererDesc/
--- docker/Centos/Maya2018Update3/installMaya2018.sh
+++ docker/Centos/Maya2018Update3/installMaya2018.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
-echo 'MAYA_INSTALL_SAS='$1
-MAYA_INSTALL_SAS=$1
+echo 'INSTALLER_SAS='$1
+INSTALLER_SAS=$1
 
 mkdir -m777 /tmp/maya2018 -p
 mkdir -m777 /tmp/maya/Maya2018 -p
@@ -9,13 +9,13 @@ mkdir -m777 /tmp/maya/Maya2018 -p
 echo '-------------------------------------------------------------------------------------'
 echo '------------------------------=Download Maya 2018=-----------------------------------'
 echo '-------------------------------------------------------------------------------------'
-azcopy --source $MAYA_INSTALL_SAS --destination /tmp/maya/Autodesk_MayaIO_2018_3_Update_Linux_64bit.run
+azcopy --source $INSTALLER_SAS --destination /tmp/maya/Autodesk_MayaIO_2018_Linux_64bit.run
 
 echo '-------------------------------------------------------------------------------------'
 echo '------------------------------=Extract and Install Maya 2018=-----------------------------------'
 echo '-------------------------------------------------------------------------------------'
-7za e /tmp/maya/Autodesk_MayaIO_2018_3_Update_Linux_64bit.run -o/tmp/maya/
-7za e /tmp/maya/Autodesk_MayaIO_2018_3_Update_Linux_64bit -o/tmp/maya/Maya2018/
+7za e /tmp/maya/Autodesk_MayaIO_2018_Linux_64bit.run -o/tmp/maya/
+7za e /tmp/maya/Autodesk_MayaIO_2018_Linux_64bit -o/tmp/maya/Maya2018/
 chmod +x /tmp/maya/Maya2018/setup.sh
 /tmp/maya/Maya2018/setup.sh 127.0.0.1
 
