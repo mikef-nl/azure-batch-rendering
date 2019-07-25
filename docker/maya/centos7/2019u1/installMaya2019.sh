8ea168703961f9e0ff59e14b200668ea52ae6e9d
David Kydd <dakydd@microsoft.com>
2019-07-25 22:05:17 +1200

    update LD_LIBRARY_PATH to correct location

--- docker/maya/centos7/2019u1/installMaya2019.sh
+++ docker/maya/centos7/2019u1/installMaya2019.sh
@@ -20,7 +20,7 @@ echo '-=Extract and Install=-'
 chmod +x /tmp/maya/Maya2019/setup.sh
 /tmp/maya/Maya2019/setup.sh 127.0.0.1
 
-sudo echo LD_LIBRARY_PATH=/usr/autodesk/mayaIO2019/bin/maya/plug-ins/xgen/lib >> /etc/environment
+sudo echo LD_LIBRARY_PATH=/usr/autodesk/mayaIO2019/plug-ins/xgen/lib >> /etc/environment
 
 cp /usr/bin/maya2019.sh /usr/bin/maya.sh
 
