73749a7e094754b1e06cca997e4b0b01de823f4c
David Kydd <dakydd@microsoft.com>
2019-07-26 13:36:15 +1200

    Revert addition of double quotes, source etc/environment after setting it

--- docker/maya/centos7/2019u1/installMaya2019.sh
+++ docker/maya/centos7/2019u1/installMaya2019.sh
@@ -20,7 +20,8 @@ echo '-=Extract and Install=-'
 chmod +x /tmp/maya/Maya2019/setup.sh
 /tmp/maya/Maya2019/setup.sh 127.0.0.1
 
-sudo echo "export LD_LIBRARY_PATH=\"/usr/autodesk/mayaIO2019/plug-ins/xgen/lib\"" >> /etc/environment
+sudo echo "export LD_LIBRARY_PATH=/usr/autodesk/mayaIO2019/plug-ins/xgen/lib" >> /etc/environment
+sudo source /etc/environment
 
 cp /usr/bin/maya2019.sh /usr/bin/maya.sh
 
